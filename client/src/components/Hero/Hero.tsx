6e0ab1eb3dcaef3c50108c4e287c97cb3700037e
cactustune <ojoshegun42@gmail.com>
2023-05-04 13:42:00 +0100

    added signup and login modal

---
+++ client/src/components/Hero/Hero.tsx
@@ -0,0 +1,48 @@
+import styled from "styled-components";
+import { Container } from "react-bootstrap";
+import ModalComponent from "../Modal/Modal";
+
+const HeroComponent = styled.header`
+  padding: 5rem 0;
+  height: 60vh;
+  background-image: url("https://images.unsplash.com/photo-1682965636882-30892f3f5565?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1771&q=80");
+  background-size: cover;
+  background-position: center;
+`;
+
+const HeaderContainer = styled.div`
+  background-color: rgb(5, 148, 112);
+  padding: 3rem;
+  color: white;
+  width: 32.5rem;
+`;
+
+const Heading = styled.h1`
+  font-size: 2.5rem;
+  margin: 1rem;
+`;
+
+const SubHeading = styled.h3`
+  margin: 1rem 0;
+  font-weight: 400;
+`;
+
+const Hero = () => {
+  return (
+    <HeroComponent>
+      <Container>
+        <HeaderContainer>
+          <Heading>Feed your mind wth the best</Heading>
+          <SubHeading>
+            Grow, Learn, Visit and become more social by booking your picnics
+            with family, friends and Coleagues
+          </SubHeading>
+          <ModalComponent text="Singup" variant="primary" />
+          <ModalComponent text="Login" variant="danger" />
+        </HeaderContainer>
+      </Container>
+    </HeroComponent>
+  );
+};
+
+export default Hero;
---
+++ client/src/components/Modal/Modal.tsx
@@ -0,0 +1,52 @@
+import { Modal, Button, InputGroup, FormControl } from "react-bootstrap";
+import { useState } from "react";
+import ModelManager from "sequelize/types/model-manager";
+import { ValidatorConstraint } from "class-validator";
+
+interface ModalProps {
+  text: string;
+  variant: "primary" | "danger" | "secondary";
+}
+
+const ModalComponent = ({ text, variant }: ModalProps) => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <Button
+        onClick={handleShow}
+        variant={variant}
+        size="lg"
+        style={{ marginRight: "1rem", padding: "0.6rem 2.5rem" }}
+      >
+        {text}
+      </Button>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header>
+          <Modal.Title>{text}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <InputGroup className="mb-3">
+            <InputGroup.Text>Email</InputGroup.Text>
+            <FormControl type="email" />
+          </InputGroup>
+          <InputGroup className="mb-3">
+            <InputGroup.Text>Password</InputGroup.Text>
+            <FormControl type="password" />
+          </InputGroup>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary">{text}</Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+};
+
+export default ModalComponent;
